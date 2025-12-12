4131b977f549d60687444c0ab10f7f591fc8397c
suraj patidar <ptdrsuraj@gmail.com>
2025-12-12 16:57:44 +0530

    add new crud functionality in my project APIVIEW

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,9 @@ from . import views
 
 urlpatterns = [
     path('students/',views.studentView),
-    path('student/<int:pk>/',views.StudentDetailVew)
+    path('student/<int:pk>/',views.StudentDetailVew),
+
+    path('employees/',views.Employees.as_view()),
+    path('employees/<int:pk>',views.EmployeeDetail.as_view()),
+
 ]
--- api/views.py
+++ api/views.py
@@ -2,14 +2,23 @@ from os.path import exists
 
 from django.core.serializers import serialize
 from rest_framework.status import HTTP_400_BAD_REQUEST
+from rest_framework.views import APIView
 
 from students.models import Student
 from .serializers import StudentSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
-
+from rest_framework.views import APIView
+from employees.models import Employee
+from . serializers import  EmployeeSerializer
+from django.http import Http404
 # Create your views here.
+
+
+
+
+#function based view with decorators
 @api_view(['GET','POST'])
 def studentView(request):
     if request.method == 'GET':
@@ -53,6 +62,56 @@ def StudentDetailVew(request,pk):
 
 
 
+
+
+#  starting class based view:
+
+class Employees(APIView):
+    def get(self,request):
+        employees = Employee.objects.all()
+        serializer = EmployeeSerializer(employees,many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+    def post(self,request):
+        serializer = EmployeeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status = HTTP_400_BAD_REQUEST)
+
+
+class EmployeeDetail(APIView):
+    def get_object(self,pk):
+        try:
+           return Employee.objects.get(pk=pk)
+        except Employee.DoesNotExist:
+            raise Http404
+
+
+
+    def get(self,request,pk):
+        employee = self.get_object(pk)
+        serializer = EmployeeSerializer(employee)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+
+    def put(self,request,pk):
+         employee = self.get_object(pk)
+         serializer = EmployeeSerializer(employee,data=request.data)
+         if serializer.is_valid():
+             serializer.save()
+             return Response(serializer.data, status=status.HTTP_200_OK)
+
+         return Response(serializer.errors,status = HTTP_400_BAD_REQUEST)
+
+
+
+    def delete(self,request,pk):
+        employee = self.get_object(pk)
+        employee.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 

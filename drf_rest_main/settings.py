d92262cab6f057ed74633a13fc4f627c48d1ed09
suraj patidar <ptdrsuraj@gmail.com>
2025-12-13 00:16:06 +0530

    using globle pagination in my project

--- drf_rest_main/settings.py
+++ drf_rest_main/settings.py
@@ -129,3 +129,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    # 'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
+'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 5,
+
+}
